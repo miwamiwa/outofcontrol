64d801b645afce30979f649d28fc5a67a4d81a2e
samjam <6parsam@gmail.com>
2020-07-11 14:13:40 -0400

    working tree gen

--- outofcontrol_game/outofcontrol/Assets/mapgen/mapgenerator.cs
+++ outofcontrol_game/outofcontrol/Assets/mapgen/mapgenerator.cs
@@ -8,50 +8,70 @@ public class mapgenerator : MonoBehaviour
 
     int gridDefinition = 20;
 
-    int[][] grid;
-    int[][][] blobs;
+    private List<List<int>> grid;
+    //  [System.NonSerialized]
+    private List<List<List<int>>> blobs;
+  //  int[][][] blobs;
 
     int nodeSpacing = 6;
-    int maxTreeCount = 20;
-    int nodeMargin = 3; // minimum space between growing nodes 
+    int maxTreeCount = 30; // maxtree per node
+    float nodeMargin = 0.8f; // minimum space between growing nodes 
+    int treeCounter = 0;
+    int blobDefinition = 0;
+    int nodeCount = 0;
 
+    float gridRatio = 0;
     // Start is called before the first frame update
     void Start()
-    {   
-        // setup arrays
-        grid = new int[gridDefinition][];
-        for (int i=0; i<gridDefinition; i++)
-        {
-            grid[i] = new int[gridDefinition];
-            for(int j=0; j<gridDefinition; j++)
-            {
-                grid[i][j] = -1;
-            }
-        }
-
-        int blobDefinition = gridDefinition / nodeSpacing +1;
-        int blobCount = blobDefinition * blobDefinition;
-        blobs = new int[blobCount][][];
-        for(int i=0; i<blobCount; i++)
-        {
-            blobs[i] = new int[maxTreeCount][];
-            for (int j = 0; j < maxTreeCount; j++)
-            {
-                blobs[i][j] = new int[2];
-                blobs[i][j][0] = -1;
-                blobs[i][j][1] = -1;
-            }
-        }
+    {
         
-
-
         Renderer rend;
         rend = GetComponent<Renderer>();
-        Debug.Log(rend.bounds);
-        // test placing something at the end
-        Instantiate(testcube, rend.bounds.center + new Vector3(rend.bounds.extents.x , 0.5f, 0f), Quaternion.identity);
+        gridRatio = rend.bounds.extents.x / gridDefinition;
+        blobDefinition = gridDefinition / nodeSpacing + 1;
+        nodeCount = blobDefinition * blobDefinition;
 
-        // setup blobs
+        grid= new List<List<int>>(20);
+        blobs = new List<List<List<int>>>(16);
+
+
+        // setup grid array
+        // syntax grid[x][y]=blobIndex;
+        //grid = new int[gridDefinition][];
+        for (int i=0; i<gridDefinition; i++)
+        {
+            grid.Add(new List<int>());
+           // grid[i] = ;
+            for (int j=0; j<gridDefinition; j++)
+            {
+                grid[i].Add(-1);
+            }
+        }
+
+        // setup blob array
+
+        // syntax blob[nodeIndex][tilelist][x,y]
+        
+     //   blobs = new int[nodeCount][][];
+        for(int i=0; i< nodeCount; i++)
+        {
+            blobs.Add(new List<List<int>>());
+
+            for (int j = 0; j < maxTreeCount; j++)
+            {
+              
+
+                blobs[i].Add(new List<int> ());
+                blobs[i][j].Add(-1);
+                blobs[i][j].Add(-1);
+            }
+        }
+
+        
+
+        
+
+        // setup blob nodes 
         int blobCounter = 0;
         for(int x=0; x<gridDefinition; x++)
         {
@@ -61,94 +81,154 @@ public class mapgenerator : MonoBehaviour
                 {
                     int offsetX = Random.Range(-2, 2);
                     int offsetY = Random.Range(-2, 2);
-                    Debug.Log(blobCounter);
+                  //  Debug.Log(blobCounter);
                     int blobX = Mathf.Clamp(x + offsetX, 0, gridDefinition);
                     int blobY = Mathf.Clamp(y + offsetY, 0, gridDefinition);
-
+                    //Debug.Log(blobX + " " + blobY);
                     blobs[blobCounter][0][0] = blobX;
                     blobs[blobCounter][0][1] = blobY;
 
+                   // Debug.Log(blobs[blobCounter][0][0]+" "+ blobs[blobCounter][0][1]);
                     grid[blobX][blobY] = blobCounter;
                     blobCounter++;
                 }
             }
         }
 
+       // Debug.Log("blob counter: " + blobCounter + ", node count: " + nodeCount);
+      //  Debug.Log(blobs[3][0][1]);
+        // grow the nodes 
+        // setup candidates array 
+        // syntax candidates[index]=[x,y];
         int[][] candidates = new int[100][];
-        for(int i=0; i<candidates.Length; i++)
+        for (int i = 0; i < candidates.Length; i++)
         {
             candidates[i] = new int[2];
         }
         int candidateCount = 0;
 
-        // grow the nodes
-        for(int i=0; i<maxTreeCount; i++)
+
+        for (int t=0; t<60; t++)
         {
-            for(int j=0; j<blobCount; j++)
+           // Debug.Log(blobs[3][0][1]);
+            if (treeCounter >= 0)
             {
 
-                for(int k=0; k<i; k++)
+                
+
+                // for each blob node
+                for (int j = 0; j < nodeCount; j++)
                 {
-                    Debug.Log(blobs[j][k][0]);
-                    if (candidateCount<100&&checkCandidate(blobs[j][k][0] + 1, blobs[j][k][1], j))
+
+                    // for each element in this blob node's tile list so far 
+                    for (int k = 0; k < 3; k++)
                     {
-                        candidates[candidateCount][0] = blobs[j][k][0] + 1;
-                        candidates[candidateCount][1] = blobs[j][k][1];
-                        candidateCount++;
+                       // Debug.Log("i: " + treeCounter + ", j: " + j + ", k: " + k);
+                      //  Debug.Log(blobs[j][k][0] + " " + blobs[j][k][1]);
+                        if (candidateCount < 100 )
+                        {
+                           // Debug.Log(blobs[3][0][1]);
+                            if (checkCandidate(blobs[j][k][0] + 1, blobs[j][k][1], j))
+                            {
+                               
+                                candidates[candidateCount][0] = blobs[j][k][0] + 1;
+                                candidates[candidateCount][1] = blobs[j][k][1];
+                                candidateCount++;
+                            }
+
+                            if (checkCandidate(blobs[j][k][0], blobs[j][k][1] + 1, j))
+                            {
+                                candidates[candidateCount][0] = blobs[j][k][0];
+                                candidates[candidateCount][1] = blobs[j][k][1] + 1;
+                                candidateCount++;
+                            }
+
+                            if (checkCandidate(blobs[j][k][0] - 1, blobs[j][k][1], j))
+                            {
+                                candidates[candidateCount][0] = blobs[j][k][0] - 1;
+                                candidates[candidateCount][1] = blobs[j][k][1];
+                                candidateCount++;
+                            }
+
+                            if (checkCandidate(blobs[j][k][0], blobs[j][k][1] - 1, j))
+                            {
+                                candidates[candidateCount][0] = blobs[j][k][0];
+                                candidates[candidateCount][1] = blobs[j][k][1] - 1;
+                                candidateCount++;
+                            }
+                        }
+
+
+
+                        // x: blobs[j][k][0]
+                        // y: blobs[j][k][1]
+
+
                     }
 
-                    if (candidateCount < 100 && checkCandidate(blobs[j][k][0], blobs[j][k][1] + 1, j))
+                    if (candidateCount > 0)
                     {
-                        candidates[candidateCount][0] = blobs[j][k][0] ;
-                        candidates[candidateCount][1] = blobs[j][k][1] + 1;
-                        candidateCount++;
-                    }
+                        int pick = Random.Range(0, candidateCount);
+                    //    Debug.Log(candidateCount);
 
-                    if (candidateCount < 100 && checkCandidate(blobs[j][k][0] - 1, blobs[j][k][1], j))
+                        grid[candidates[pick][0]][candidates[pick][1]] = j;
+                        blobs[j][t][0] = candidates[pick][0];
+                        blobs[j][t][1] = candidates[pick][1];
+                        Vector2 randomOffset =0.4f * Random.insideUnitCircle;
+                        Instantiate(
+                                testcube,
+                                rend.bounds.center + new Vector3(
+                                    1+randomOffset.x + 0.9f*rend.bounds.extents.x * (2f * candidates[pick][0] / gridDefinition - 1f),
+                                    0.5f,
+                                    1+randomOffset.y + 0.9f * rend.bounds.extents.z * (2f * candidates[pick][1] / gridDefinition - 1f)),
+                                Quaternion.identity
+                                );
+                        //   Debug.Log("candidate count: "+candidateCount+", pick " + pick + ", x: " + blobs[j][i][0] + ", y: " + blobs[j][i][1]);
+                        candidateCount = 0;
+                    }
+                    
+                }
+            }
+
+        }
+
+
+
+        treeCounter++;
+
+            if (treeCounter >= maxTreeCount)
+            {
+
+                // get ground tile bounds 
+                
+                // test placing something at the end
+                Instantiate(testcube, rend.bounds.center + new Vector3(rend.bounds.extents.x, 0.5f, 0f), Quaternion.identity);
+
+                for (int i = 0; i < blobs.Count; i++)
+                {
+                    for (int j = 0; j < blobs[i].Count; j++)
                     {
-                        candidates[candidateCount][0] = blobs[j][k][0] - 1;
-                        candidates[candidateCount][1] = blobs[j][k][1];
-                        candidateCount++;
-                    }
+                    Vector2 randomOffset = 3f*Random.insideUnitCircle;
+                       // Debug.Log("make tree");
+                        GameObject newcube = Instantiate(
+                            testcube,
+                            rend.bounds.center + new Vector3(
+                                randomOffset.x + rend.bounds.extents.x * (2 * blobs[i][j][0] / gridDefinition - 1),
+                                0.5f,
+                                randomOffset.y + rend.bounds.extents.z * (2 * blobs[i][j][1] / gridDefinition - 1)),
+                            Quaternion.identity
+                            );
 
-                    if (candidateCount < 100 && checkCandidate(blobs[j][k][0], blobs[j][k][1] - 1, j))
-                    {
-                        candidates[candidateCount][0] = blobs[j][k][0] ;
-                        candidates[candidateCount][1] = blobs[j][k][1] - 1;
-                        candidateCount++;
-                    }
-
-
-                    // x: blobs[j][k][0]
-                    // y: blobs[j][k][1]
-
+                    
 
                 }
-
-                int pick = Random.Range(0, candidateCount);
-                grid[candidates[pick][0]][candidates[pick][1]] = j;
-                blobs[j][i][0] = candidates[pick][0];
-                blobs[j][i][1] = candidates[pick][1];
-                candidateCount = 0;
             }
-        }
 
-        for(int i=0; i<blobs.Length; i++)
-        {
-            for(int j=0; j<blobs[i].Length; j++)
-            {
-               // Debug.Log("boom "+ blobs[i][j][0]+" " + blobs[i][j][1]);
-                
-                Instantiate(
-                    testcube, 
-                    rend.bounds.center + new Vector3(
-                        rend.bounds.extents.x * (2*blobs[i][j][0]/gridDefinition -1),
-                        0.5f,
-                        rend.bounds.extents.z * (2 * blobs[i][j][1] / gridDefinition - 1)), 
-                    Quaternion.identity
-                    );
+                treeCounter = -1;
             }
-        }
+        
+
+
     }
 
     bool checkCandidate(int x, int y, int blob)
@@ -158,30 +238,37 @@ public class mapgenerator : MonoBehaviour
 
         // if candidate out of bounds
         if (x < 0 || x >= gridDefinition || y < 0 || y >= gridDefinition) rejected = true;
-
+      //  Debug.Log(rejected);
         // if grid tile is occupied 
         if (!rejected)
         {
             if (grid[x][y] != -1) rejected = true;
         }
-
+        // Debug.Log(rejected);
         // if any of the other blobs are in range 
-
-        for(int i=0; i<blobs.Length; i++)
+        //Debug.Log(blobs.Count);
+        for (int i=0; i<blobs.Count; i++)
         {
-            for(int j=0; j<blobs[i].Length; j++)
+           // Debug.Log(blobs[i].Count);
+            for(int j=0; j<blobs[i].Count; j++)
             {
+              //  Debug.Log(blobs[i][j][0]);
                 Vector2 distance = new Vector2(blobs[i][j][0], blobs[i][j][1]) - new Vector2(x, y);
-                if (distance.magnitude <= nodeMargin && i!=blob) rejected = true;
+               // Debug.Log("d: " + distance.magnitude);
+                if (distance.magnitude*gridRatio < nodeMargin && i!=blob) rejected = true;
             }
         }
-
+      //  Debug.Log(rejected);
+       // Debug.Log(x+", "+ y + ", " + blob);
+       // Debug.Log(rejected);
         return !rejected;
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+
         
     }
 }
