5d27fc422c3875748e963971d5e75fcc353c69a3
samjam <6parsam@gmail.com>
2020-07-12 02:47:03 -0400

    basic models

---
+++ outofcontrol_game/outofcontrol/Assets/Robots/animateBigRobot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animateBigRobot : MonoBehaviour
+{
+    public GameObject BigSaw;
+    public GameObject SmallSaw;
+
+    float bigSawCounter = 0f;
+    float smallSawCounter = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        BigSaw.transform.localEulerAngles = new Vector3(-90f, bigSawCounter, 0f);
+        SmallSaw.transform.localEulerAngles = new Vector3(90f, smallSawCounter, 0f);
+
+        bigSawCounter -= 4.0f;
+        smallSawCounter += 2.5f;
+    }
+}
---
+++ outofcontrol_game/outofcontrol/Assets/Robots/animateSmallRobot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animateSmallRobot : MonoBehaviour
+{
+
+    public GameObject saw;
+    float sawCounter = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        saw.transform.localEulerAngles = new Vector3(-90f, sawCounter, 0f);
+    //    SmallSaw.transform.localEulerAngles = new Vector3(90f, smallSawCounter, 0f);
+
+        sawCounter -= 3.0f;
+       // smallSawCounter += 2.5f;
+    }
+}
